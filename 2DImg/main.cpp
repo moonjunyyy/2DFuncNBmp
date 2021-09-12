0f2f22a38989530bb5be9a4bf87db9a72c6a45ce
문준영 <moonjunyyy@naver.com>
2021-09-12 16:27:45 +0900

    spint4 이미지 변형

--- 2DImg/main.cpp
+++ 2DImg/main.cpp
@@ -13,13 +13,13 @@ int main(int argc, char* argv[])
 
 	mybmp = new unsigned char[width * height * 3];
 	
-	for (int i = 0; i < 300; i++)
+	for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < 200; j++)
+		for (int j = 0; j < width; j++)
 		{
-			mybmp[3 * (i * 200 + j) + 0] = 0;
-			mybmp[3 * (i * 200 + j) + 1] = 0;
-			mybmp[3 * (i * 200 + j) + 2] = i > 255 ? 255 : i;
+			mybmp[3 * (i * width + j) + 0] = 0;
+			mybmp[3 * (i * width + j) + 1] = 0;
+			mybmp[3 * (i * width + j) + 2] = i > 255 ? 255 : i;
 		}
 	}
 
@@ -28,4 +28,55 @@ int main(int argc, char* argv[])
 	WriteBmp(NameofFile, mybmp, 200, 300);
 
 	delete[] mybmp;
+
+	//Sprint 4
+	unsigned char* bmpB, * bmpRed, * bmpGreen, * bmpBlue, * bmpMono;
+	char NameofFile2[] = "sunflower.bmp";
+	bmpB = ReadBmp(NameofFile2, &width, &height);
+	
+	bmpRed = new unsigned char[width * height * 3];
+	bmpGreen = new unsigned char[width * height * 3];
+	bmpBlue = new unsigned char[width * height * 3];
+	bmpMono = new unsigned char[width * height * 3];
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			bmpRed	[3 * (i * width + j) + 0] = 0;
+			bmpRed	[3 * (i * width + j) + 1] = 0;
+			bmpRed	[3 * (i * width + j) + 2] = bmpB[3 * (i * width + j) + 2];
+
+			bmpGreen[3 * (i * width + j) + 0] = 0;
+			bmpGreen[3 * (i * width + j) + 1] = bmpB[3 * (i * width + j) + 1];
+			bmpGreen[3 * (i * width + j) + 2] = 0;
+
+			bmpBlue	[3 * (i * width + j) + 0]= bmpB[3 * (i * width + j) + 0];
+			bmpBlue	[3 * (i * width + j) + 1] = 0;
+			bmpBlue	[3 * (i * width + j) + 2] = 0;
+
+			short Bright
+				= bmpB[3 * (i * width + j) + 0]
+				+ bmpB[3 * (i * width + j) + 1]
+				+ bmpB[3 * (i * width + j) + 2];
+			Bright = (double)Bright / 3.;
+
+			bmpMono	[3 * (i * width + j) + 0] = Bright;
+			bmpMono	[3 * (i * width + j) + 1] = Bright;
+			bmpMono	[3 * (i * width + j) + 2] = Bright;
+		}
+	}
+
+	char NameofFile3[] = "bmpRed.bmp";
+	char NameofFile4[] = "bmpGreen.bmp";
+	char NameofFile5[] = "bmpBlud.bmp";
+	char NameofFile6[] = "bmpMono.bmp";
+
+	WriteBmp(NameofFile3, bmpRed, width, height);
+	WriteBmp(NameofFile4, bmpGreen, width, height);
+	WriteBmp(NameofFile5, bmpBlue, width, height);
+	WriteBmp(NameofFile6, bmpMono, width, height);
+
+	delete[] bmpB;
+	delete[] bmpRed, bmpGreen, bmpBlue, bmpMono;
 }
