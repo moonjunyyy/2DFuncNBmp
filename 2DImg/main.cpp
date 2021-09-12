92b6e17894d402c6fec9e1a3af28251bb5137a4f
문준영 <moonjunyyy@naver.com>
2021-09-12 15:50:46 +0900

    Sprint 1 동적배열에 값 할당하기

---
+++ 2DImg/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//Sprint 1
+	int width = 200, height = 300;
+	unsigned char* mybmp;
+
+	mybmp = new unsigned char[width * height * 3];
+	
+	for (int i = 0; i < 300; i++)
+	{
+		for (int j = 0; j < 200; j++)
+		{
+			mybmp[3 * (i * 200 + j) + 0] = 0;
+			mybmp[3 * (i * 200 + j) + 1] = 0;
+			mybmp[3 * (i * 200 + j) + 2] = i > 255 ? 255 : i;
+		}
+	}
+
+	delete[] mybmp;
+}
